aa06f9cb75951a0dc7bd3d5304094ed5a81c96ce
blorn <fnark@hotmail.com>
2023-05-26 11:15:10 -0400

    more

--- cmd/simple-service/main.go
+++ cmd/simple-service/main.go
@@ -10,10 +10,10 @@ func main() {
 	// hello world
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello world from Blorn!\n")
+		io.WriteString(w, "Hello awesome world from Blorn!\n")
 	}
 
 	http.HandleFunc("/hello", helloHandler)
 	log.Println("Listening for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000",nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
