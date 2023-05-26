13fbc95c155f8d7de8af8a3ef68be53ac78ed795
blorn <fnark@hotmail.com>
2023-05-26 10:37:49 -0400

    First commit

---
+++ cmd/simple-service/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// hello world
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello world from Blorn!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Listening for requests at http://localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000",nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blorn/simple-go-service
+
+go 1.17
